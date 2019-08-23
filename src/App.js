a204ca1fe31f655a5fbd13f7076e75b5d6d6289e
jonjon <jonjonclark@gmail.com>
2019-08-23 06:24:40 +0200

    refactor Search component to functional component with hooks

--- src/App.js
+++ src/App.js
@@ -19,16 +19,6 @@ class App extends Component {
     alert: null
   };
 
-  /*   async componentDidMount() {
-    this.setState({ loading: true });
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    this.setState({ users: res.data, loading: false });
-  } */
-
   // Search github users
   searchUsers = async text => {
     this.setState({ loading: true });
