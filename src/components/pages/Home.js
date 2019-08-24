618df02ff70e0fdc4669a8b04bc5d91d27203dc1
jonjon <jonjonclark@gmail.com>
2019-08-24 10:49:42 +0200

    Not found page added

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Search from '../users/Search';
+import Users from '../users/Users';
+
+const Home = () => (
+  <Fragement>
+    <Search />
+    <Users />
+  </Fragement>
+);
+
+export default Home;
